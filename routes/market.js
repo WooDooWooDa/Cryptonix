538d82037725005b1fab29cb13086e73e44afea6
WooDooWooDa <jeremie-bouchard@hotmail.fr>
2021-11-21 19:31:30 -0500

    transaction refactor views

--- routes/market.js
+++ routes/market.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const {Transaction} = require("../models/Transaction");
+const {Crypto} = require("../models/Cryptos");
 var router = express.Router();
 
 /* GET home page. */
@@ -7,6 +9,18 @@ router.get('/', function(req, res, next) {
         res.redirect('/login');
     }
     res.render('market', {title: "Market", account: req.session.account});
+});
+
+router.get('/:crypto', function (req, res) {
+    if (!req.session.isLogged) {
+        res.redirect('/login');
+    }
+    const currentCrypto = req.params.crypto;
+    res.render('singleMarket', {
+        title: req.params.crypto,
+        account: req.session.account,
+        crypto: currentCrypto
+    });
 });
 
 module.exports = router;
--- routes/wallets.js
+++ routes/wallets.js
@@ -1,17 +1,31 @@
 var express = require('express');
 var router = express.Router();
 const {CanvasRenderService} = require('chartjs-node-canvas');
+const {Transaction} = require("../models/Transaction");
+const {Crypto} = require("../models/Cryptos");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     if (!req.session.isLogged) {
         res.redirect('/login');
     }
-    res.render('wallets', {title: "Wallets", account: req.session.account});
+    res.render('wallets', {
+        title: "Wallets",
+        account: req.session.account,
+        lastTransaction: new Transaction(Crypto.ETH, 0.001)
+    });
 });
 
 router.get('/:crypto', function (req, res) {
-    res.render('singleWallet', {title: req.params.crypto, account: req.session.account});
+    if (!req.session.isLogged) {
+        res.redirect('/login');
+    }
+    const transactions = [new Transaction(Crypto.BTC, 0.002), new Transaction(Crypto.ETH, 0.01), new Transaction(Crypto.ADA, 100233), new Transaction(Crypto.ETH, 0.0043)];
+    res.render('singleWallet', {
+        title: req.params.crypto,
+        account: req.session.account,
+        transactions: transactions
+    });
 });
 
 module.exports = router;
